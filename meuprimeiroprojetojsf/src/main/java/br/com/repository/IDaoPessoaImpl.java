cfb3ecb76e1d870bdbd2dae1b1eec2739338a34f
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2022-11-20 11:02:13 -0300

    projeto

--- meuprimeiroprojetojsf/src/main/java/br/com/repository/IDaoPessoaImpl.java
+++ meuprimeiroprojetojsf/src/main/java/br/com/repository/IDaoPessoaImpl.java
@@ -21,6 +21,7 @@ import br.com.jpautil.JPAUtil;
 public class IDaoPessoaImpl implements IDaoPessoa,Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
 	@Inject
 	private EntityManager entityManager;
 
